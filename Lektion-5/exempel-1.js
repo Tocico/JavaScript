23f1f2a9c103ba18388fd574dcc554da133d5902
Tocico <kunotoshiko17@gmail.com>
2018-10-04 10:10:31 +0200

    !

---
+++ Lektion-5/exempel-1.js
@@ -0,0 +1,18 @@
+// Arbeta med arrayer
+
+//Skapa en array
+let cars1 = [];
+
+//Skapa en array som innehåller data
+let cars2 =['Saab', 'Volvo','BMW'];
+//Detta är en array-literal
+
+//Skapa en array med en konstruktor
+let cars3 = new Array (1,2,3);
+let cars4 = new Array (); // en tom array
+
+//Att komma åt data
+console.log(cars2[0]); 
+console.log(cars2[1]); 
+console.log(cars2[2]); 
+console.log(cars2[,]); 
