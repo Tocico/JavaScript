df7f5ec8ba0a5d47b5a7be3661f8c20f4d4ba6e2
Tocico <kunotoshiko17@gmail.com>
2018-10-15 10:49:35 +0200

    newww

---
+++ Lektion-8/exempel-1.js
@@ -0,0 +1,26 @@
+// Arbeta med konstruktorer
+
+// Skapa ett nytt objekt med hjälp av en konstruktor
+let toshiko = new Object();
+//Egenskaper
+toshiko.name = 'TOshiko kuno';
+toshiko.age = 29;
+
+//Metoder
+toshiko.info = function(){
+    console.log(this.name + this.age);
+}
+
+//En metod som returnerar en sträng
+toshiko.info = function(){
+    return `${this.name}  ${this.age} år gammal`;
+}
+
+// Använd objektets egenskaper
+console.log(toshiko.name);
+
+// Använd objektets metoder
+toshiko.info();
+// console.log(toshiko.info()); Undefined
+
+console.log(toshiko.info());
